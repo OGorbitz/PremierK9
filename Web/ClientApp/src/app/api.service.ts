d80c50f7ec725cf29f19c20e74d984efc4c42231
Ozzie Gorbitz <ozziegorbitz@gmail.com>
2022-05-15 01:40:16 -0500

    Finally got everything to play nice. Web project completely overhauled. Angular app displaying properly using SPA Proxy during debug

---
+++ Web/ClientApp/src/app/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+
+const API_URL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class APIService {
+
+  constructor() { }
+}
---
+++ Web/ClientApp/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+
+  //TODO: change this to the production API url
+  apiUrl: 'http://localhost:3000/api'
+};
